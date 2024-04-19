5d813e4dd7be3167bcaa0ae7c09e19b7ba3398b1
Abdelnissoul <abdelnissoul205@gmail.com>
2024-04-19 15:33:07 -0700

    pup

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 #!/usr/bin/puppet
 # Let's manifest Puppet to install Flask from pip3
-package {'flask':
-	ensure	 => '2.1.0',
-	provider => 'pip'
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  creates => '/usr/local/lib/python3.8/dist-packages/Flask',
+  path    => ['/usr/bin'],
 }
