58242ad5dd98d7a829ab936aa643afc182ddb617
Abdelnissoul <abdelnissoul205@gmail.com>
2024-05-20 14:46:06 +0100

    exporting to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,47 +1,24 @@
 #!/usr/bin/python3
-""" 
-Export employee TODO list progress to CSV format.
-"""
-
+""" Exporting api to csv"""
 import csv
 import requests
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: ./1-export_to_CSV.py <employee_id>")
-        sys.exit(1)
+    user = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
+    res = requests.get(url_user)
+    """getting them"""
+    user_name = res.json().get('username')
+    task = url_user + '/todos'
+    res = requests.get(task)
+    tasks = res.json()
 
-    employee_id = sys.argv[1]
-
-    try:
-        employee_id = int(employee_id)
-    except ValueError:
-        print("Employee ID must be an integer")
-        sys.exit(1)
-
-    # Fetch user data
-    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print(f"User with ID {employee_id} not found.")
-        sys.exit(1)
-    user_data = user_response.json()
-    username = user_data.get('username')
-
-    # Fetch tasks data
-    tasks_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-    tasks_response = requests.get(tasks_url)
-    tasks = tasks_response.json()
-
-    # Write to CSV file
-    csv_filename = f'{employee_id}.csv'
-    with open(csv_filename, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    with open('{}.csv'.format(user), 'w') as csvfile:
         for task in tasks:
-            csvwriter.writerow([
-                employee_id,
-                username,
-                task.get('completed'),
-                task.get('title')
-            ])
+            completed = task.get('completed')
+            """Completed"""
+            title_task = task.get('title')
+            """Done"""
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user, user_name, completed, title_task))
