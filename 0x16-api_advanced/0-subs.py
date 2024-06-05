465853ece17725c7da98fd16734040577f4c2605
Abdelnissoul <abdelnissoul205@gmail.com>
2024-06-05 19:48:25 +0100

    fixing bugs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 """
-making queries subscribers on a given Reddit subreddit.
+Query the number of subscribers on a given Reddit subreddit.
 """
+
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Gives the total number of subscribers on the subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """Returns the total number of subscribers for the subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
-    answer = requests.get(url, headers=headers, allow_redirects=False)
-    if answer.status_code == 200:
-        data = answer.json()
-        subscribers = data['data']['subscribers']
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data.get("data", {}).get("subscribers", 0)
         return subscribers
     else:
         return 0
+
+
+# Test the function
+if __name__ == "__main__":
+    subreddit = input("Enter the name of the subreddit: ")
+    subscribers = number_of_subscribers(subreddit)
+    print(f"The number of subscribers on r/{subreddit}: {subscribers}")
