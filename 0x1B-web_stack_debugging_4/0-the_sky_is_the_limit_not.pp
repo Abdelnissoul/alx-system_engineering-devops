0c3f9e2cdc1327581a696693d49f40fef0724dc1
Abdelnissoul <abdelnissoul205@gmail.com>
2024-06-12 22:14:59 +0100

    0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# Puppet manifest to increase the maximum number of open
+#files for Nginx and restart the service
+
+exec { 'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
